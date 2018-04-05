11d9b91cdfc73402cbc3f20f9800c958cad0ca83
Sajjan Singh Mehta <sajjan.s.mehta@gmail.com>
2018-04-04 20:26:37 -0700

    finished signup interface

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -9,84 +9,90 @@ declare var StellarSdk: any;
   styleUrls: ['signup.component.less']
 })
 export class SignupComponent {
-  
+
   ISAAC_ASSET_NAME: string = 'ISAAC';
   ISAAC_ISSUING_ACCOUNT: string = 'GD5SQKW4MDDAQDUSW7CVJJCMF5JWBXBY5RN7XYHDR4L6FF2NAU4I7GOQ';
-  
+
   server: any;
-  
+
   running: boolean;
   loadingText: string;
   success: boolean;
   error: string;
-  
+
   pair: any;
   account: any;
 
   constructor(private http: HttpClient) {
     StellarSdk.Network.useTestNetwork();
     this.server = new StellarSdk.Server('https://horizon-testnet.stellar.org');
-    
+
     this.running = false;
   }
 
   private createAccount() {
     this.running = true;
-	this.loadingText = 'Creating Your Account...';
-    
+    this.loadingText = 'Creating Your Account...';
+
     var isaacToken = new StellarSdk.Asset(this.ISAAC_ASSET_NAME, this.ISAAC_ISSUING_ACCOUNT);
-    
+
     // Local variable to avoid scrope issues
-	var self = this;
+    var self = this;
     self.pair = StellarSdk.Keypair.random();
-	
+
     this.http.get('https://friendbot.stellar.org?addr='+ self.pair.publicKey())
       .subscribe((data) => {
         console.log(data);
-      
+
         self.server.loadAccount(self.pair.publicKey()).then(function(account) {
           console.log('Received details for account: '+ self.pair.publicKey());
           self.account = account;
-          
+
           account.balances.forEach(function(balance) {
             console.log('Type:', balance.asset_type, ', Balance:', balance.balance);
           });
-          
+
           // Generate trust transaction
           var transaction = new StellarSdk.TransactionBuilder(account)
             .addOperation(StellarSdk.Operation.changeTrust({
               asset: isaacToken
             }))
             .build();
-          
+
           transaction.sign(self.pair);
-          
-		  self.loadingText = 'Generating Trust Transaction...';
+
+          self.loadingText = 'Generating Trust Transaction...';
           console.log('Submitting trust transaction...');
-          
+
           return self.server.submitTransaction(transaction);
         })
         .then(function(result) {
-		  console.log('Trust transaction successful');
-		  console.log(result);
-		  
+          console.log('Trust transaction successful');
+          console.log(result);
+
           self.server.loadAccount(self.pair.publicKey()).then(function(account) {
-			console.log('Received details for account after transaction');
-			self.account = account;
-			self.success = true;
-			self.running = false;
-			
-			account.balances.forEach(function(balance) {
-			  console.log('Type:', balance.asset_type, ', Balance:', balance.balance);
-			});
-		  });
-        })
-        .catch(function(error) {
-          console.error('Error!', error);
+          console.log('Received details for account after transaction');
+          self.account = account;
+          self.success = true;
+          self.running = false;
+
+          account.balances.forEach(function(balance) {
+            console.log('Type:', balance.asset_type, ', Balance:', balance.balance);
+          });
         });
-      },
-      (err) => {
-        
+      })
+      .catch(function(error) {
+        console.error('Error!', error);
+        self.error = 'Unable to execute trust transaction!';
+        self.success = true;
+        self.running = false;
       });
+    },
+    (error) => {
+      console.error('Error!', error);
+      self.error = 'Unable to generate Stellar account!';
+      self.success = true;
+      self.running = false;
+    });
   }
 }
